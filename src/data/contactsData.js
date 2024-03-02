dd2fe7aa83e843ff57061a37db2465e5f95cee3e
sohaibhas <maliksohaib0987@gmail.com>
2024-03-03 00:44:50 +0500

    first comit

---
+++ src/data/contactsData.js
@@ -0,0 +1,12 @@
+export const contactsData = {
+  email: "sohaibhassan0000@gmail.com",
+  phone: "+923404892471",
+  address: "DHA phase 1 , Lahore Pakistan",
+  github: "https://github.com/sohaibhas",
+  facebook: "https://www.facebook.com/abusaid.riyaz/",
+  linkedIn: "https://www.linkedin.com/in/sohaibhassan-/",
+  twitter: "https://twitter.com/said7388",
+  medium: "https://medium.com/@abusaid-bd",
+  stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
+  devUsername: "said7388",
+};
---
+++ src/data/educationData.js
@@ -0,0 +1,23 @@
+export const educationData = [
+  {
+    id: 1,
+    institution: "Comsats University Islambad",
+    course: "Bachelor Degree",
+    startYear: "2023",
+    endYear: "Present",
+  },
+  {
+    id: 2,
+    institution: "Superior Collage Siakot",
+    course: "Higher Secondary",
+    startYear: "2016",
+    endYear: "2018",
+  },
+  {
+    id: 3,
+    institution: "Govt Higher Secondary School",
+    course: "Secondary School",
+    startYear: "2014",
+    endYear: "2016",
+  },
+];
