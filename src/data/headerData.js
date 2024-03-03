fab0e5452976f62cc41acd322cadf2bea681d811
sohaibhas <maliksohaib0987@gmail.com>
2024-03-03 14:21:11 +0500

    changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,6 +5,6 @@ export const headerData = {
     "Hello! I am SOHAIB HASSAN, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
   image: "https://i.ibb.co/THF71QH/new-pi-removebg-preview.png",
   imagebw: "https://i.ibb.co/THF71QH/new-pi-removebg-preview.png",
-//   resumePdf:
-//     "https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing",
+  //   resumePdf:
+  //     "https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing",
 };
